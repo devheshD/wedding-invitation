c2093053464f2dc434c412ea9aa4bfd657265938
rasung-ki <rlfktjd!8624>
2025-09-06 21:39:54 +0900

    신랑 신부 이름 변경

--- src/const.ts
+++ src/const.ts
@@ -24,8 +24,8 @@ export { dayjs }
 export const WEDDING_DATE = dayjs.tz("2024-08-24 13:00", "Asia/Seoul")
 export const HOLIDAYS = [15]
 
-export const LOCATION = "서울대학교 연구공원 웨딩홀"
-export const LOCATION_ADDRESS = "서울시 관악구 관악로 1, 연구공원 본관 1층"
+export const LOCATION = "더파티움!@!"
+export const LOCATION_ADDRESS = "더파티움 주소가 들어갈곳"
 
 export const SHARE_ADDRESS = LOCATION
 export const SHARE_ADDRESS_TITLE = LOCATION
@@ -35,8 +35,8 @@ export const WEDDING_HALL_POSITION = [126.9594982, 37.4657134]
 export const NMAP_PLACE_ID = 13321741
 export const KMAP_PLACE_ID = 8634826
 
-export const BRIDE_FULLNAME = "정지원"
-export const BRIDE_FIRSTNAME = "지원"
+export const BRIDE_FULLNAME = "송지연"
+export const BRIDE_FIRSTNAME = "지연"
 export const BRIDE_TITLE = "장녀"
 export const BRIDE_FATHER = "정상원"
 export const BRIDE_MOTHER = "박윤정"
@@ -61,8 +61,8 @@ export const BRIDE_INFO = [
   },
 ]
 
-export const GROOM_FULLNAME = "남주호"
-export const GROOM_FIRSTNAME = "주호"
+export const GROOM_FULLNAME = "기라성"
+export const GROOM_FIRSTNAME = "라성"
 export const GROOM_TITLE = "차남"
 export const GROOM_FATHER = "남현태"
 export const GROOM_MOTHER = "김현영"
